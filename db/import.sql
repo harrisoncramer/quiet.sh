eef657809b5b7870763b7953cf78ea2edfbb8224
Harrison Cramer <harrisoncramer@gmail.com>
2021-07-29 08:11:28 -0400

    Modified PSQL table creation.

--- db/import.sql
+++ db/import.sql
@@ -1,10 +1,10 @@
-CREATE TABLE users (
-  github_id integer NOT NULL PRIMARY KEY,
-  login varchar(100) NOT NULL,
-  avatar_url varchar(100),
-  repos_url varchar(100) NOT NULL,
-  token varchar(75)
-);
+/* CREATE TABLE users ( */
+/*   github_id integer NOT NULL PRIMARY KEY, */
+/*   login varchar(100) NOT NULL, */
+/*   avatar_url varchar(100), */
+/*   repos_url varchar(100) NOT NULL, */
+/*   token varchar(75) */
+/* ); */
 CREATE TABLE reports (
   id integer,
   description text,
@@ -13,5 +13,7 @@ CREATE TABLE reports (
   user_id integer REFERENCES users(github_id),
   is_gitleaks boolean,
   number_of_secrets integer,
-  time_of_execution date
+  time_of_execution timestamp,
+  is_exposed boolean DEFAULT false,
+  exposed_count integer DEFAULT 0
 )
